d5c3a852bd91c641ed19f8344bbdee34de59a0be
kananbayramli <bayramli-kenan@mail.ru>
2024-04-22 17:58:19 +0400

    Dynamic category menu

---
+++ Concrete/EfCore/EfCoreCategoryRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+using shopapp.data.Abstract;
+using shopapp.entity;
+
+namespace shopapp.data.Concrete.EfCore
+{
+    public class EfCoreCategoryRepository : EfCoreGenericRepository<Category, ShopContext>, ICategoryRepository
+    {
+        public List<Category> GetPopularCategories()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
