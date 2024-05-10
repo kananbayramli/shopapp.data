4b1d30aad356729f915a84933974478e3bbd120a
kananbayramli <bayramli-kenan@mail.ru>
2024-05-10 18:50:20 +0400

    AddToBasket configuring

---
+++ Abstract/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using shopapp.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopapp.data.Abstract
+{
+    public interface ICartRepository : IRepository<Cart>
+    {
+    }
+}
---
+++ Migrations/20240510115620_AddingCartEntities.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace shopapp.data.Migrations
+{
+    public partial class AddingCartEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CartId = table.Column<string>(type: "TEXT", nullable: true),
+                    CartId1 = table.Column<int>(type: "INTEGER", nullable: true),
+                    ProductId = table.Column<string>(type: "TEXT", nullable: true),
+                    ProductId1 = table.Column<int>(type: "INTEGER", nullable: true),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartItems_Carts_CartId1",
+                        column: x => x.CartId1,
+                        principalTable: "Carts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CartItems_Products_ProductId1",
+                        column: x => x.ProductId1,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_CartId1",
+                table: "CartItems",
+                column: "CartId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductId1",
+                table: "CartItems",
+                column: "ProductId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+        }
+    }
+}
