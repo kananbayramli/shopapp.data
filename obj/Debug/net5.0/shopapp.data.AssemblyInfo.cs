07ce92eee39f3ee125351a7cfafc9a559fb95b30
kananbayramli <bayramli-kenan@mail.ru>
2024-05-10 13:55:11 +0400

    Admin Seed Data

--- obj/Debug/net5.0/shopapp.data.AssemblyInfo.cs
+++ obj/Debug/net5.0/shopapp.data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopapp.data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b39cd3b40867a174bf5a8cfe7aa00bd0b19e5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopapp.data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopapp.data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
