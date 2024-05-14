79b684dfa30960e24e95f85609c652d38ac6ce8d
kananbayramli <bayramli-kenan@mail.ru>
2024-05-15 00:13:24 +0400

    Order List for user

--- Concrete/EfCore/EfCoreOrderRepository.cs
+++ Concrete/EfCore/EfCoreOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using shopapp.data.Abstract;
+﻿using Microsoft.EntityFrameworkCore;
+using shopapp.data.Abstract;
 using shopapp.entity;
 using System;
 using System.Collections.Generic;
@@ -8,8 +9,24 @@ using System.Threading.Tasks;
 
 namespace shopapp.data.Concrete.EfCore
 {
-    public class EfCoreOrderRepository : EfCoreGenericRepository<Order, ShopContext> , IOrderRepository
+    public class EfCoreOrderRepository : EfCoreGenericRepository<Order, ShopContext>, IOrderRepository
     {
+        public List<Order> GetOrders(string userId)
+        {
+            using (var context = new ShopContext())
+            {
+                var orders = context.Orders
+                                        .Include(i => i.OrderItems)
+                                        .ThenInclude(i => i.Product)
+                                        .AsQueryable();
 
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    orders = orders.Where(i => i.UserId == userId);
+                }
+
+                return orders.ToList();
+            }
+        }
     }
 }
