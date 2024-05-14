951c5a2b7a593846fd193e1819e76e4220c9226a
kananbayramli <bayramli-kenan@mail.ru>
2024-05-14 14:49:05 +0400

    Registering an Order / With Iyzipay

---
+++ Concrete/EfCore/EfCoreOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using shopapp.data.Abstract;
+using shopapp.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopapp.data.Concrete.EfCore
+{
+    public class EfCoreOrderRepository : EfCoreGenericRepository<Order, ShopContext> , IOrderRepository
+    {
+
+    }
+}
